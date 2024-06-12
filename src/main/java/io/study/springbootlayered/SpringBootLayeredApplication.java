35ff17e9554e3e697114a077e7fd1b92d61f6bdf
이윤선 <yoonseon3@gmail.com>
2024-06-12 18:48:42 +0900

    init project

---
+++ src/main/java/io/study/springbootlayered/SpringBootLayeredApplication.java
@@ -0,0 +1,13 @@
+package io.study.springbootlayered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLayeredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLayeredApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/study/springbootlayered/SpringBootLayeredApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.springbootlayered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLayeredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
